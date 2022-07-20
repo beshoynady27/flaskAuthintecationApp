1181840be02df89e2ab2aa4c589e06adae3083c9
beshoynady27 <beshoynady27@gmail.com>
2022-07-20 20:00:13 +0200

    using SQLAlchemy instead of SQLite

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+#app.config('SECRET_KEY') = 'secret_key'
 
 
 login_manager = flask_login.LoginManager()
@@ -145,90 +145,3 @@ def loged_in() :
 def admin_panil():
 
     return render_template('admin_panil.html', '''patients = patients''')
-
-@app.route('/add_new_patient')
-def add_new_patient():
-
-    return render_template('add_new_patient.html')
-
-
-#recieve the supmetted form add_new_patient.html and add the results to the database
-@app.route('/added_patient', methods=["GET", "POST"])
-def added_patient():
-
-    patient_name = request.form['patient_name']
-    gender = request.form['gender']
-    birth_year = request.form['birth_year']
-    adress = request.form['adress']
-    phone_number = request.form['phone_number']
-    email = request.form['email']
-
-    patient = Patient(name=patient_name, gender=gender, birth_year=birth_year, adress=adress, email=email, phone_number=phone_number)
-    db.session.add(patient)
-    db.session.commit()
- 
-    return redirect('/admin_panil')
-
-
-@app.route('/patient_file', methods = ['GET', 'POST'])
-def patient_file():
-    if request.method == 'POST':
-        selected_patient_name = request.form["n"]
-       # procedure_type = request.form['procedure_type']
-        #teeth = request.form['teeth']
-       # price = request.form['price']
-
-
-    #cur.execute('INCERT INTO procedures(procedure_type, teeth, price) VALUES(?, ?, ?);',(procedure_type, teeth, price))
-   # patient_info= cur.execute('SELECT * FROM patients ')
-   # patient_info= cur.execute('SELECT * FROM patients WHERE patient_name = (?);', (selected_patient_name,))
-
-
-    return render_template('patient_file.html', '''patient_info = patient_info''', selected_patient_name=selected_patient_name)
-
-
-@app.route('/added_procedure', methods = ['GET', 'POST'])
-def added_procedure():
-    if request.method == 'POST':
-        procedure_type = request.form['procedure_type']
-        teeth = request.form['teeth']
-        price = request.form['price']
-        patient_name = request.form['patient_name']
-        prucedure_date = datetime.now()
-    
-    '''
-    patient_id = cur.execute('SELECT id FROM patients WHERE patient_name = (?);', (patient_name,)).fetchone()['id']
-    cur.execute('INSERT INTO procedures(procedure_type, tooth, price, procedure_date, patient_id) VALUES(?, ?, ?, ?, ?);',
-                                                                        (procedure_type, teeth, price, prucedure_date, patient_id))
-    patient_info= cur.execute('SELECT * FROM patients WHERE patient_name = (?);', (patient_name,))
-    '''
-    
-
-
-
-
-    return render_template('patient_file_after_added_procedure.html', '''patient_info = patient_info''', '''patient_id=patient_id''')
-
-
-@app.route('/add_new_doctor')
-def add_new_doctor():
-
-    return render_template('add_new_doctor.html')
-
-@app.route('/added_doctor', methods= ['GET', 'POST'])
-def added_doctor():
-    if request.method == 'POST':
-        name = request.form['name']
-        gender = request.form['gender']
-        speciality = request.form['speciality']
-        email = request.form['email']
-        password = request.form['doctor_password']
-    '''
-    cur.execute('INSERT INTO doctors (doctor_name, gender, speciality, email, doctor_password) VALUES(?, ?, ?, ?, ?)',
-                    (name, gender, speciality, email, password))
-    '''
-
-
-    return redirect('/admin_panil')
-
-    
